28b1272cbec1c13a04e882ef6eea7064ca88a77c
MiguelLameiras <mlameirasmlameiras@gmail.com>
2024-03-22 13:14:13 +0000

    O can bus está a correr agora num core independente. Cada pico consegue identificar of ids dos outros picos ligados

--- Project/Project.ino
+++ Project/Project.ino
@@ -5,46 +5,50 @@
 #include "interface.hh"
 #include "mcp2515.h"
 #include "can.h"
-#include <hardware/flash.h> 
+#include <hardware/flash.h>
 #include <hardware/gpio.h>
+#include "utils.h"
 
 
 //*************Global Variables**************
-float x_ref = 50.0; //Reference value in LUX (default = 50) 
-float r; // Reference value in voltage - obtained from x_ref 
-float y; // Measured value in voltage (0-4095) - obvained from converting x to voltage
-float u; // Control signal in PWM (0-4095) - applied in the actuator
-unsigned long int delta_control; // Timer variables
-volatile bool timer_fired {false};
+float x_ref = 50.0;               //Reference value in LUX (default = 50)
+float r;                          // Reference value in voltage - obtained from x_ref
+float y;                          // Measured value in voltage (0-4095) - obvained from converting x to voltage
+float u;                          // Control signal in PWM (0-4095) - applied in the actuator
+unsigned long int delta_control;  // Timer variables
+volatile bool timer_fired{ false };
 struct repeating_timer timer;
 time_t initial_time = millis();
 
 
 //*************CAN BUS**************
-uint8_t this_pico_flash_id[8], node_address;
+uint8_t pico_flash_id[8];
 struct can_frame canMsgTx, canMsgRx;
-unsigned long counterTx {0}, counterRx {0};
+unsigned long counterTx{ 0 }, counterRx{ 0 };
 MCP2515::ERROR err;
 unsigned long time_to_write = millis();
-unsigned long write_delay {1000};
-const byte interruptPin {20};
-volatile byte data_available {false};
+unsigned long write_delay{ 1000 };
+const byte interruptPin{ 20 };
+volatile byte data_available{ false };
+uint8_t canintf_save{ 0 };
+uint8_t eflg_save{ 0 };
+bool detected_errors{ false };
 
-MCP2515 can0 {spi0, 17, 19, 16, 18, 10000000}; // CS, INT, SCK, MISO, MOSI
+MCP2515 can0{ spi0, 17, 19, 16, 18, 10000000 };  // CS, INT, SCK, MISO, MOSI
 
-
+//List to hold the ids of the other picos
+uint8_t id_list[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 //*************Functions**************
-bool my_repeating_timer_callback(struct repeating_timer *t)
-{
-  if(!timer_fired)
+bool my_repeating_timer_callback(struct repeating_timer *t) {
+  if (!timer_fired)
     timer_fired = true;
   return true;
 }
 
 float analog_low_pass_filter() {
   // Declare an array of floats
-  static float arr[AVGR_FILTER_SAMPLE_NUM] = {0.0};
+  static float arr[AVGR_FILTER_SAMPLE_NUM] = { 0.0 };
 
   // Analog read
   for (int i = 0; i < AVGR_FILTER_SAMPLE_NUM; i++) {
@@ -73,30 +77,27 @@ void read_interrupt(uint gpio, uint32_t events) {
 
 
 //*************Objects**************
-CLuminaire lum; //Luminaire object
-CPID my_pid(TIME_SAMPLING, 1, 1, 1, 0, 1, 10); //Sampling time = 10ms, K = 1, b = 1, Ti = 1, Td = 0, Tt = 1, N = 10
+CLuminaire lum;                                 //Luminaire object
+CPID my_pid(TIME_SAMPLING, 1, 1, 1, 0, 1, 10);  //Sampling time = 10ms, K = 1, b = 1, Ti = 1, Td = 0, Tt = 1, N = 10
 
 //*****************Setup****************
+// The setup for the main core
 void setup() {
   //Get unique flash and node address
-  flash_get_unique_id(this_pico_flash_id);
-  node_address = this_pico_flash_id[7];
-
-  //Initialize CAN bus
-  can0.reset();
-  can0.setBitrate(CAN_1000KBPS); 
-  can0.setNormalMode();
-  gpio_set_irq_enabled_with_callback(interruptPin, GPIO_IRQ_EDGE_FALL, true, &read_interrupt); //Enable interrupt
-  time_to_write = millis() + write_delay;
+  rp2040.idleOtherCore();
+  //flash calls are unsafe if two cores are operating
+  flash_get_unique_id(pico_flash_id);
+  id_list[0] = pico_flash_id[6];
+  rp2040.resumeOtherCore();
 
   //Setup serial communication and IO pins
   Serial.begin(SERIAL_BAUD);
-  analogReadResolution(ADC_RESOLUTION); //2^12 bits = 4095
-  analogWriteFreq(PWM_FREQ); //PWM frequency = 60kHz
-  analogWriteRange(DAC_RANGE); //PWM range = 4095 
+  analogReadResolution(ADC_RESOLUTION);  //2^12 bits = 4095
+  analogWriteFreq(PWM_FREQ);             //PWM frequency = 60kHz
+  analogWriteRange(DAC_RANGE);           //PWM range = 4095
 
   //Setup timer for sampling time
-  add_repeating_timer_ms(-10, my_repeating_timer_callback, NULL, &timer); //10ms period
+  add_repeating_timer_ms(-10, my_repeating_timer_callback, NULL, &timer);  //10ms period
 
   //Get unique board ID
   int len = 2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1;
@@ -105,7 +106,7 @@ void setup() {
 
   //Initialize luminaire object, according to board type
   lum.init_lum(id, len);
-   
+
   //Calibrate open loop gain G
   lum.G = lum.calibrate_open_loop_gain_G();
 
@@ -115,77 +116,88 @@ void setup() {
 
   // Update control system parameters, according to the new parameters values
   my_pid.update_parameters(lum.G, lum.H, lum.tau);
-  
+
   //Compute coefficients
   my_pid.compute_coefficients();
 
   //Compute inicial reference value in voltage
   r = lum.voltage_func(x_ref);
-} 
+}
 
+// The setup for the core dedicated to the can bus
+void setup1() {
+  //Initialize CAN bus
+  can0.reset();
+  can0.setBitrate(CAN_1000KBPS);
+  can0.setNormalMode();
+  gpio_set_irq_enabled_with_callback(interruptPin, GPIO_IRQ_EDGE_FALL, true, &read_interrupt);  //Enable interrupt
+  time_to_write = millis() + write_delay;
+}
 
 //*************Main Loop*********************
 void loop() {
 
+  can_frame frm;
+  uint32_t msg;
+  uint8_t b[4];
+
   //Check for user input
   handle_serial(lum, my_pid, x_ref, r, y, u, initial_time);
 
-  //Check for CAN bus data - send
-  if( millis() - time_to_write > 5000) { //Send message every 5 seconds
-    canMsgTx.can_id = node_address;
-    canMsgTx.can_dlc = 8;
-    unsigned long div = counterTx*10;
-    for( int i = 0; i < 8; i++ )
-      canMsgTx.data[7-i]='0'+((div/=10)%10);
-
-    //Send message
-    err = can0.sendMessage(&canMsgTx);
-    Serial.print("Error: ");
-    Serial.println(err);
-    Serial.print("Sending message ");
-    Serial.print( counterTx );
-    Serial.print(" from node ");
-    Serial.print( node_address, HEX );
-    Serial.print(" : ");
-    for (int i=0 ; i < canMsgTx.can_dlc ; i++)
-      Serial.print((char) canMsgTx.data[ i ]);
-
-    Serial.println(" ");
+  //If it is time yo send message, send message to other core
+  if (millis() >= time_to_write) {
+    b[3] = ICC_WRITE_DATA;  // Identifier
+    b[2] = id_list[0];      // Which id is sending the msg? 0 index corresponds to me
+    b[0] = counterTx;
+    b[1] = (counterTx >> 8);
+    rp2040.fifo.push(bytes_to_msg(b));
+    Serial.print(">>>>>> Sending ");
+    print_message(counterTx, b[2], b[2], counterTx);
     counterTx++;
-    time_to_write = millis();
-
+    time_to_write = millis() + write_delay;
   }
 
-  //Check for CAN bus data - receive
-  if (data_available) {
+  //check incoming data in fifo queue
+  if (rp2040.fifo.pop_nb(&msg)) {
+    msg_to_bytes(msg, b);
+    if (b[3] == ICC_READ_DATA) {
+      uint16_t val = msg;
+      Serial.print("<<<<<< Received ");
+      print_message(counterRx++, id_list[0], b[2], val);
 
-    Serial.println("Data available!");
-
-    //Read message
-    err=can0.readMessage(&canMsgRx);    
-    Serial.print("Error: ");
-    Serial.println(err);
-
-    //Print message
-    Serial.print("Received message number "); Serial.print(counterRx++);
-    Serial.print(" from node "); Serial.print(canMsgRx.can_id, HEX);
-    Serial.print(" : ");
-    for (int i=0 ; i < canMsgRx.can_dlc ; i++)
-      Serial.print((char) canMsgRx.data[ i ]);
-    Serial.println(" ");
-
-    //Set data_available flag to false
-    data_available = false;
+      //Update the list of known ids
+      id_list[find(id_list, b[2])] = b[2];
+      //Temporary print id_list
+      Serial.print("\nIds I am aware: ");
+      for (int i = 0; i < sizeof(id_list) / sizeof(id_list[0]); i++) {
+        Serial.print(id_list[i]);
+        Serial.print(" ");
+      }
+      Serial.println("");
+    } else if (b[3] == ICC_ERROR_DATA) {
+      //print_can_errors(b[1],b[0]);
+      if (b[0] & 0b11111000) {  //RX0OV | RX1OV | TXBO | TXEP | RXEP
+        eflg_save = b[0];
+        canintf_save = b[1];
+        can0.clearRXnOVRFlags();
+        can0.clearInterrupts();
+        detected_errors = true;
+      }
+    }
+    if (detected_errors) {
+      Serial.println(" ***** Detected errors in past operations *****");
+      //print_can_errors(canintf_save, eflg_save);
+    }
   }
-  
+
   //Check for new sampling time
-  if(timer_fired) {
+  if (timer_fired) {
 
     //Check time
     delta_control = -micros();
 
     //Read analog input value
-    y = analog_low_pass_filter(); 
+    y = analog_low_pass_filter();
 
     //Compute H and tau, according to the new LUX reference (x_ref)
     lum.H = lum.compute_open_loop_gain_H(x_ref);
@@ -199,27 +211,67 @@ void loop() {
 
     //Compute reference value in voltage
     r = lum.voltage_func(x_ref);
-    
+
     //Compute control signal (in PWM from 0 to 4095)
     u = my_pid.compute_control(r, y);
 
     //Write analog output
-    analogWrite(LED_PIN, int(u)); 
+    analogWrite(LED_PIN, int(u));
 
     //Housekeeping
     my_pid.housekeeping(r, y);
 
     // Store last minute data (PWM stored in percentage, r in LUX and y in volts)
-    lum.store_luminaire_data(u*100/4095, lum.lux_func(y), x_ref);
+    lum.store_luminaire_data(u * 100 / 4095, lum.lux_func(y), x_ref);
 
     //Reset timer
     timer_fired = false;
 
     //Check time elapsed for control - if it is greater than the sampling time, print a warning
-    if ((delta_control + micros()) > TIME_SAMPLING*1000000) { 
+    if ((delta_control + micros()) > TIME_SAMPLING * 1000000) {
       Serial.println("Sampling time exceeded! Check control loop!");
     }
-  }  
+  }
+}
+
+void loop1() {
+  can_frame frm;
+  uint32_t msg;
+  uint8_t b[4];
+
+  //reading the can-bus and writing the fifo
+  //if interrupt has happened (message received)
+  if (data_available) {
+    data_available = false;
+    uint8_t irq = can0.getInterrupts();
+    if (irq & MCP2515::CANINTF_RX0IF) {
+      can0.readMessage(MCP2515::RXB0, &frm);
+      //Push message to other core
+      rp2040.fifo.push_nb(can_frame_to_msg(&frm));
+    }
+    if (irq & MCP2515::CANINTF_RX1IF) {
+      can0.readMessage(MCP2515::RXB1, &frm);
+
+      rp2040.fifo.push_nb(can_frame_to_msg(&frm));
+    }
+    uint8_t err = can0.getErrorFlags();
+    rp2040.fifo.push_nb(error_flags_to_msg(irq, err));
+  }
+
+  //send message to the can bus
+  if (rp2040.fifo.pop_nb(&msg)) {
+    msg_to_bytes(msg, b);
+    if (b[3] == ICC_WRITE_DATA) {
+      frm.can_id = b[2];
+      frm.can_dlc = 2;
+      frm.data[1] = b[1];
+      frm.data[0] = b[0];
+      can0.sendMessage(&frm);
+    }
+    uint8_t irq = can0.getInterrupts();
+    uint8_t err = can0.getErrorFlags();
+    rp2040.fifo.push_nb(error_flags_to_msg(irq, err));
+  }
 }
 
 
---
+++ Project/utils.h
@@ -0,0 +1,100 @@
+// Function that returns index of element in array, else returns the index of available slot
+uint8_t find(uint8_t arr[], uint8_t id) {
+  int availableSlot = -1;
+  int size = sizeof(arr) / sizeof(arr[0]);
+  for (int i = 0; i < size; ++i) {
+    if (arr[i] == id) {
+      return i;  // If id is found, return its index
+    } else if (arr[i] == 0 && availableSlot == -1) {
+      availableSlot = i;  // Update available slot index if found
+    }
+  }
+  return availableSlot;  // Return available slot index if id not found
+}
+
+//Command IDs for intercore communication
+enum inter_core_cmds {
+  //From core1 to core0: contains data read (16 bit)
+  ICC_READ_DATA = 1,
+  // From core0 to core1: contains data to write (16 bit)
+  ICC_WRITE_DATA = 2,
+  // From core1 to core0: contains regs CANINTF, EFLG
+  ICC_ERROR_DATA = 3
+};
+
+/////////////////////////////////////////////////////
+//Inter-core message definitions and helper functions
+/////////////////////////////////////////////////////
+
+//Packs 4 bytes into an unsigned int
+uint32_t bytes_to_msg(uint8_t *b) {
+  uint32_t b0{ b[0] }, b1{ b[1] }, b2{ b[2] }, b3{ b[3] };
+  return b0 + (b1 << 8) + (b2 << 16) + (b3 << 24);
+}
+
+//Unpacks an unsigned int into its constituting 4 bytes
+void msg_to_bytes(uint32_t msg, uint8_t *bytes) {
+  bytes[0] = msg;
+  bytes[1] = (msg >> 8);
+  bytes[2] = (msg >> 16);
+  bytes[3] = (msg >> 24);
+}
+
+//Packs the CAN frame contents into an unsigned int
+uint32_t can_frame_to_msg(can_frame *frm) {
+  uint8_t b[4];
+  b[3] = ICC_READ_DATA;
+  b[2] = frm->can_id;
+  b[1] = frm->data[1];
+  b[0] = frm->data[0];
+  return bytes_to_msg(b);
+}
+
+//Packs the CAN error flags into an unsigned int
+uint32_t error_flags_to_msg(uint8_t canintf, uint8_t eflg) {
+  uint8_t b[4];
+  b[3] = ICC_ERROR_DATA;
+  b[2] = 0;
+  b[1] = canintf;
+  b[0] = eflg;
+  return bytes_to_msg(b);
+}
+
+//Prints the contents of a CAN message
+void print_message(int number, int node, int id, int val) {
+  Serial.print(number);
+  Serial.print(" at node ");
+  Serial.print(node, HEX);
+  Serial.print(" with id ");
+  Serial.print(id, HEX);
+  Serial.print(" : ");
+  Serial.println(val);
+}
+
+/////////////////////////////////////////////////////
+//Helper function to print error flags
+/////////////////////////////////////////////////////
+
+char canintf_str[]{ "| MERRF | WAKIF | ERRIF | TX2IF | TX0IF | TX1IF | RX1IF | RX0IF | " };
+char eflg_str[]{ "| RX1OV | RX0OV | TXBO | TXEP | RXEP | TXWAR | RXWAR | EWARN | " };
+void print_can_errors(uint8_t canintf, uint8_t eflg) {
+  Serial.println("-----------------------------------------------------------------");
+  Serial.println(canintf_str);
+  Serial.print("| ");
+  for (int bit = 7; bit >= 0; bit--) {
+    Serial.print(" ");
+    Serial.write(bitRead(canintf, bit) ? '1' : '0');
+    Serial.print(" | ");
+  }
+  Serial.println("");
+  Serial.println("-----------------------------------------------------------------");
+  Serial.println(eflg_str);
+  Serial.print("| ");
+  for (int bit = 7; bit >= 0; bit--) {
+    Serial.print(" ");
+    Serial.write(bitRead(eflg, bit) ? '1' : '0');
+    Serial.print(" | ");
+  }
+  Serial.println("");
+  Serial.println("-----------------------------------------------------------------");
+}
